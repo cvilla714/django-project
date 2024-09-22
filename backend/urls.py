2a51fa367704eb6f2bfd937a4d4fc5debb9e7fb2
Cosmel Villalobos <cosmelvg@gmail.com>
2024-09-21 23:20:56 -0400

    Main changes after moving api to backend: - inside the INSTALLED_APPS list in the settings.py file, addED the new app name: 'backend.api' - inside the urls.py file, added the new from backend.api.schema import schema - inside the apps.py changed the name = "backend.api" from name = "api"

--- backend/urls.py
+++ backend/urls.py
@@ -17,7 +17,17 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from graphene_django.views import GraphQLView
+from django.views.decorators.csrf import csrf_exempt
+from .views import home  # Import the home view
+from backend.api.schema import schema
+from django.http import HttpResponse
+
+def home_view(request):
+    return HttpResponse("Welcome to the Django-GraphQL App")
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('', home_view),  # Root URL
+    path('admin/', admin.site.urls),
+    path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema))),
 ]
