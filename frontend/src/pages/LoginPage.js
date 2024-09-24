70096cadc9f2588bfa33220a00710ced467223b3
Cosmel Villalobos <cosmelvg@gmail.com>
2024-09-23 22:33:29 -0400

    Added loginPage styling

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -2,6 +2,7 @@
 import React, { useState } from 'react';
 import { useMutation, gql } from '@apollo/client';
 import { useNavigate } from 'react-router-dom';
+import './LoginPage.css';
 
 const LOGIN_USER = gql`
   mutation LoginUser($username: String!, $password: String!) {
@@ -30,19 +31,17 @@ function LoginPage() {
   };
 
   return (
-    <div>
-      <h2>Login</h2>
-      <form onSubmit={handleLogin}>
-        <div>
-          <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
-        </div>
-        <div>
-          <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
-        </div>
-        <button type="submit">Login</button>
-      </form>
-      {error && <p>Error logging in</p>}
-      {loading && <p>Loading...</p>}
+    <div className="login-page">
+      <div className="login-container">
+        <h2 className="login-heading">Login</h2>
+        <form className="login-form" onSubmit={handleLogin}>
+          <input className="login-input" type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
+          <input className="login-input" type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+          <button className="login-button" type="submit">
+            Login
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
